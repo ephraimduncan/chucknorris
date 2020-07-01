b9954be2d6a6cbad417ffaf36ebeff0193c23663
Ephraim Atta-Duncan <[ephraimduncan68@gmail.com]>
2020-07-01 22:46:15 +0000

    Commit Messages Update

--- index.js
+++ index.js
@@ -1,37 +1,18 @@
-// const TwitterBot = require('node-twitterbot').TwitterBot;
-// const config = require('./config');
+const TwitterBot = require('node-twitterbot').TwitterBot;
+const config = require('./config');
 const { default: Axios } = require('axios');
-// const Bot = new TwitterBot(config);
-
-// const jokeRequest = Axios.get(
-//   'http://api.icndb.com/jokes/random?limitTo=[nerdy]',
-// );
-
-// Axios.all([jokeRequest])
-//   .then(
-//     Axios.spread((...responses) => {
-//       const jokeResponse = responses[0].data.value.joke;
-//       const Tweet = () => {
-//         return `${jokeResponse}\n\n#100DaysOfCode #Code #Java #javascript #DEVCommunity #programming #programmingmemes`;
-//       };
-//       Bot.tweet(Tweet());
-//     }),
-//   )
-//   .catch((errors) => {
-//     console.log(errors);
-//   });
+const Bot = new TwitterBot(config);
 
 async function postJoke() {
   try {
     const jokeResponse = await Axios.get('http://whatthecommit.com/index.txt');
-    // jokeResponse.data = `"${jokeResponse.data}"`;
     const Tweet = () => {
       return `New Commit Message!\n\n\`\`${jokeResponse.data} \n#100DaysOfCode #javascript #DEVCommunity #programming`;
     };
-    // Bot.tweet(Tweet());
-    console.log(Tweet());
+    Bot.tweet(Tweet());
+    console.log(jokeResponse.data);
   } catch (error) {
-    // Bot.tweet(error);
+    Bot.tweet(error);
     console.log(error);
   }
 }
