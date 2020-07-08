84494fd5b5903b16aa1833ece7328a0bb7237ac6
Ephraim Atta-Duncan <[ephraimduncan68@gmail.com]>
2020-07-08 21:46:25 +0000

    Never Program Mad

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const TwitterBot = require('node-twitterbot').TwitterBot;
 const config = require('./config');
 const Bot = new TwitterBot(config);
-const npm = require('./npm');
 const { randReal } = require('luckyy');
+const fs = require('fs');
+
+let npm = fs.readFileSync('./npm.txt', 'utf8').split('\r\n');
 
 const Tweet = () => {
-  return `😀😀\n\nNPM is now \n${
-    npm[randReal.rand(572)]
+  return `😀😀\n\nWhat does n-p-m stand for? \n${
+    npm[randReal.rand(840)]
   }\n\n#100DaysOfCode #javascript #DEVCommunity #programming #nodejs #npm`;
 };
 
 Bot.tweet(Tweet());
-console.log(npm[randReal.rand(572)]);
+console.log(npm[randReal.rand(840)]);
