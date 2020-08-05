88ff39bbb4129856e480b3b54b2091a4f2de824b
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2020-08-05 23:48:48 +0000

    update: finalize shower thought

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-// const TwitterBot = require('node-twitterbot').TwitterBot;
-// const config = require('./config');
+const TwitterBot = require('node-twitterbot').TwitterBot;
+const config = require('./config');
 const { default: Axios } = require('axios');
 const { rand } = require('luckyy').randReal;
-// const Bot = new TwitterBot(config);
+const Bot = new TwitterBot(config);
 
 async function postJoke() {
   try {
     const jokeResponse = await Axios.get(
       'https://www.reddit.com/r/Showerthoughts/new.json'
     );
-    // const Tweet = () => {
-    //   return `New Commit Message!\n\n\`\`${jokeResponse.data} \n#100DaysOfCode #javascript #DEVCommunity #programming`;
-    // };
+    const Tweet = () => {
+      return `New Thought!\n\n\`\`${
+        jokeResponse.data.data.children[rand(25)].data.title
+      } \n#programming #javascript #Showerthoughts`;
+    };
 
+    Bot.tweet(Tweet());
     console.log(jokeResponse.data.data.children[rand(25)].data.title);
-    // Bot.tweet(Tweet());
-    // console.log("");
   } catch (error) {
-    // Bot.tweet(error);
+    Bot.tweet(error);
     console.log(error);
   }
 }
