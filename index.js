f0395ac6917e1e39c28f88383e8b6f43d7621bad
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2020-08-27 00:35:00 +0000

    chore: Add Reddit images

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
-const TwitterBot = require('node-twitterbot').TwitterBot;
-const config = require('./config');
-const { default: Axios } = require('axios');
-const Bot = new TwitterBot(config);
+const Twitter = require('twitter');
+const fetch = require('node-fetch');
+const request = require('request');
+const { rand } = require('luckyy').randReal;
 
-async function postJoke() {
-  try {
-    const jokeResponse = await Axios.get('http://whatthecommit.com/index.txt');
-    const Tweet = () => {
-      return `New Commit Message!\n\n\`\`${jokeResponse.data} \n#100DaysOfCode #javascript #DEVCommunity #programming`;
-    };
-    Bot.tweet(Tweet());
-    console.log(jokeResponse.data);
-  } catch (error) {
-    Bot.tweet(error);
-    console.log(error);
-  }
+if (process.env.NODE_ENV === 'developent') {
+  require('dotenv').config();
 }
 
-postJoke();
+const client = new Twitter({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+});
+
+function sendImageToTwitter(imageBuffer, title) {
+  // Adapted from this example - https://github.com/desmondmorris/node-twitter/tree/master/examples#media
+  client.post('media/upload', { media: imageBuffer }, (error, media) => {
+    if (error) {
+      console.error('Something went wrong uploading image...😫', error);
+    } else {
+      client
+        .post('statuses/update', {
+          status: `${title}\n\n#100DaysOfCode #javascript`,
+          media_ids: media.media_id_string,
+        })
+        .then((resp) => console.log('New Tweet'))
+        .catch((error) =>
+          console.error('Something went wrong posting to Twitter...😒', error)
+        );
+    }
+  });
+}
+
+fetch('https://www.reddit.com/r/ProgrammerHumor/new.json')
+  .then((res) => res.json())
+  .then((json) => json.data.children)
+  .then((children) => {
+    let randomNumber = rand(children.length);
+    return [
+      children[randomNumber].data.url_overridden_by_dest,
+      children[randomNumber].data.title,
+    ];
+  })
+  .then(([imageURL, title]) => {
+    request.get(imageURL, (err, res, body) => {
+      sendImageToTwitter(body, title);
+    });
+  })
+  .catch((error) =>
+    console.error('There was an error fetching a shibe...😑', error)
+  );
