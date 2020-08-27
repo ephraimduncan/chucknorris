c9cb4ea27781d6f75d9af8b91d60106dc93980c5
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2020-08-27 00:41:42 +0000

    chore: add workflow

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ if (process.env.NODE_ENV === 'developent') {
 }
 
 const client = new Twitter({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  consumer_key: process.env.BOT_CONSUMER_KEY,
+  consumer_secret: process.env.BOT_CONSUMER_SECRET,
+  access_token_key: process.env.BOT_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET,
 });
 
 function sendImageToTwitter(imageBuffer, title) {
